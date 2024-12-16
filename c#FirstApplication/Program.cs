696aa0db06c88015ec539027628a92f54f9b1d25
ugur- <ugur-@SAMSUNG>
2024-12-16 21:53:56 +0300

    Proje dosyası ekle.

---
+++ c#FirstApplication/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ugur ARICAN");
+Console.WriteLine("Patika bootcamp'ine IT sektöründe başarılı bir yazılımcı olmak için başvurdum.");
